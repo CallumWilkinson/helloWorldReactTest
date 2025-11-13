f856e8326ecbd78fbad0b66271b819ac06f4e79b
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2025-11-13 23:15:28 +1100

    feat: added a prop to header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/header.jsx";
+
 export default function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <Header
+        title="Hello React World"
+        subtitle="This is a reuseable react component"
+      ></Header>
       <p>This is my first clean React project.</p>
     </div>
   );
